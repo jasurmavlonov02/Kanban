b49134eda7b25b352feab1165d9c062835b35d68
shahriyor <shahriyorbaxtiyorov007@gmail.com>
2022-08-01 21:02:39 +0500

    changes in models

--- apps/users/models/users.py
+++ apps/users/models/users.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import Model, DateTimeField, BooleanField, CharField, ForeignKey, TextField, EmailField, \
-    PositiveIntegerField, IntegerField, ImageField, ManyToManyField, CASCADE, IntegerChoices , FileField
+    IntegerField, ImageField, ManyToManyField, CASCADE, IntegerChoices, FileField
 
 
 class BaseModel(Model):
@@ -18,6 +18,7 @@ class DeletedModel(Model):
     class Meta:
         abstract = True
 
+
 class Role(Model):
     'CEO'
     'Branch'
@@ -27,25 +28,22 @@ class Role(Model):
     name = CharField(max_length=255)
 
 
-class User(AbstractUser , BaseModel ,DeletedModel ):
-
+class User(AbstractUser, BaseModel, DeletedModel):
     first_name = CharField(max_length=20)
     last_name = CharField(max_length=20)
     username = CharField(max_length=100)
     phone_number = CharField(max_length=12)
     photo = ImageField(upload_to='media/')
     email = EmailField()
-    role =ManyToManyField('Role' , related_name='role')
+    role = ManyToManyField('Role', related_name='role')
 
 
-
-class Project(DeletedModel , BaseModel):
+class Project(DeletedModel, BaseModel):
     title = CharField(max_length=225)
     code = CharField(max_length=100, default=f'{title}-{len(title)}')
 
 
 class Task(Model):
-
     class Priority(IntegerChoices):
         easy = 'easy'
 
@@ -55,22 +53,20 @@ class Task(Model):
     title = CharField(max_length=100)
     text = TextField()
     point = CharField(max_length=100)
-    to_user = ForeignKey('User', CASCADE , 'user')
+    to_user = ForeignKey('User', CASCADE, 'user')
     priority = IntegerField(choices=Priority.choices, null=True, blank=True)
     status = IntegerField(choices=Status.choices, null=True, blank=True)
-    author = ForeignKey('User' , CASCADE , null=True)
+    author = ForeignKey('User', CASCADE, null=True)
     step = IntegerField()
 
 
-class Comment(DeletedModel , BaseModel):
+class Comment(DeletedModel, BaseModel):
     message = TextField()
     file = FileField(upload_to='media/')
-    task_fk = ForeignKey('Task' , CASCADE , null=True)
+    task_fk = ForeignKey('Task', CASCADE, null=True)
     # FILE =
 
     # comment_fornkey
     # Step:
     # index
     # unique
-
-
