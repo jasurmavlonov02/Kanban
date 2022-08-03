61d619fdc8ecb777bf735947df43af992c39875b
jasurmavlonov02 <108140941+jasurmavlonov02@users.noreply.github.com>
2022-08-03 21:59:48 +0500

    serializers.py va views.py tayyor

--- apps/users/views/users.py
+++ apps/users/views/users.py
@@ -1,9 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from apps.users.models import User, Task, Project
 from apps.users.serializers.users import UserModelSerializer, RegisterModelSerializer, TaskModelSerializer, \
--- root/settings.py
+++ root/settings.py
@@ -136,5 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S.%fZ",
+
+
 }
